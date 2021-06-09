24ecb3685418a9d4fcb5b94b88bc169de0a01cfd
John Marczak <johnnyboyct@hotmail.com>
2021-06-08 21:29:48 -0400

    Why..

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -13,7 +13,14 @@ export default function HomePage() {
     <>
       <div>
         <GlobalStyle />
-        <h1>Some test... Create a page to display a draggable shape </h1>
+        <h1>Some test...</h1>
+        <h6>
+          Create a web page using React to display a draggable shape in the
+          middle of the screen. The shape should change color as it moves from
+          the center to the edge of the screen. Allow user to choose the type of
+          shape from a dropdown list. The list should be populated on page load
+          via API call.
+        </h6>
         <Shapes position={{ x: 20, y: 20 }} radius={20} />
       </div>
     </>
